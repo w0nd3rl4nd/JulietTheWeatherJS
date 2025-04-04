2af2d1fff6aff69fb8206e2cf4771396d51a9eb3
Alice Aliaga <alicealiagamoreno@gmail.com>
2025-04-04 09:09:33 +0200

    Create visuals and a testing function

---
+++ src/code/tests.js
@@ -0,0 +1,53 @@
+import WeatherBox from './weatherbox.js';
+
+function enableVisualTesting () {
+    console.log('Loaded For Testing')
+
+    const weatherBox = new WeatherBox(document.querySelector('#weatherbox'));
+
+    // Just to have clean record of possible weather names
+    const WEATHER_TYPES = [
+        'rainy',
+        'cloudy',
+        'sunandclouds',
+        'moonandclouds',
+        'sun',
+        'moon'
+    ];
+
+    const testCases = [
+        // Freezing temperatures
+        { weather: 'moon', temp: -5 },        // Freezing clear night
+        { weather: 'moonandclouds', temp: 3 },// Freezing cloudy night
+        { weather: 'rainy', temp: 2 },        // Freezing rain
+        
+        // Cold temperatures
+        { weather: 'cloudy', temp: 8 },       // Chilly cloudy day
+        { weather: 'sunandclouds', temp: 12 },// Cool partly cloudy
+        
+        // Mild temperatures
+        { weather: 'sun', temp: 18 },         // Mild sunny day
+        { weather: 'cloudy', temp: 20 },      // Mild overcast
+        
+        // Warm temperatures
+        { weather: 'sunandclouds', temp: 25 },// Warm partly cloudy
+        { weather: 'sun', temp: 28 },         // Hot sunny day
+        
+        // Extreme temperatures
+        { weather: 'moon', temp: 32 },        // Hot clear night
+        { weather: 'rainy', temp: -10 },      // Extreme cold rain
+        { weather: 'sun', temp: 45 }          // Heat wave
+    ];
+
+    let caseIndex = 0;
+
+    weatherBox.updateWeather(testCases[0].weather, testCases[0].temp);
+
+    setInterval(() => {
+        caseIndex = (caseIndex + 1) % testCases.length;
+        const current = testCases[caseIndex];
+        weatherBox.updateWeather(current.weather, current.temp);
+    }, 2000);
+};
+
+export default enableVisualTesting
---
+++ src/code/weatherbox.js
@@ -0,0 +1,57 @@
+class WeatherBox {
+    constructor(elementWeatherBox) {
+        this.element = elementWeatherBox;
+    }
+
+    updateWeather(weather, temperature) {
+        const weatherLower = weather.toLowerCase();
+        const weatherData = this._getWeatherData(weatherLower);
+        const thermometer = this._getThermometerImage(temperature);
+        
+        this._logParameters(weatherLower, temperature);
+
+        this.element.innerHTML = `
+            <img id="weatherdrawing" src="../graphic_res/weather/${weatherData.image}.png">
+            <p id="weathertext">${weatherData.text}</p>
+            <p id="temperaturetext">${temperature} ºC</p>
+            <img id="thermometerdrawing" src="../graphic_res/thermometer/${thermometer}.png">
+        `;
+    }
+
+    _getWeatherData(weather) {
+        const baseText = (weather === 'moonandclouds' || weather === 'moon') 
+            ? 'Tonight it is' 
+            : 'Today it is';
+        
+        const weatherMap = {
+            rainy: { image: 'Rainy', text: 'RAINY' },
+            cloudy: { image: 'Cloudy', text: 'CLOUDY' },
+            sunandclouds: { image: 'SunAndClouds', text: 'CLOUDY' },
+            moonandclouds: { image: 'MoonAndClouds', text: 'CLOUDY' },
+            sun: { image: 'Sun', text: 'SUNNY' },
+            moon: { image: 'Moon', text: 'CLEAR' }
+        };
+        
+        return {
+            image: weatherMap[weather].image,
+            text: `${baseText}<br><strong>${weatherMap[weather].text}</strong>`
+        };
+    }
+
+    _getThermometerImage(temp) {
+        if (temp < 5) return 'ThermometerFreezing';
+        if (temp < 15) return 'ThermometerLow';
+        if (temp < 22) return 'ThermometerMid';
+        if (temp < 30) return 'ThermometerHigh';
+        return 'ThermometerMelting';
+    }
+
+    _logParameters(weather, temperature) {
+        console.log(`Weather Update Received:
+        - Type: ${weather}
+        - Temperature: ${temperature}°C
+        [${new Date().toLocaleTimeString()}]`);
+    }
+}
+
+export default WeatherBox
