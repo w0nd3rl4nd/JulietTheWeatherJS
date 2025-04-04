e18c020417cc2a52471a9788f3532a71a3d1a443
Alice Aliaga <alicealiagamoreno@gmail.com>
2025-04-04 09:09:09 +0200

    Remove preload elements and activate testing

--- src/code/script.js
+++ src/code/script.js
@@ -1,12 +1,15 @@
-const { ipcRenderer } = require('electron')
-console.log('Loaded Script')
+import WeatherBox from './weatherbox.js';
+import enableVisualTesting from './tests.js'
 
-document.getElementById('minimize').addEventListener('click', () => {
-    console.log('Minimize!~')
-    ipcRenderer.invoke("minimize");
-})
+enableVisualTesting();
 
-document.getElementById('close').addEventListener('click', () => {
-    console.log('Close!~')
-    ipcRenderer.invoke("close");
-})
+// async function updateWeatherDisplay() {
+//     const { weather, temperature } = await fetchWeather();
+//     weatherBox.updateWeather(weather, temperature);
+// }
+
+// // Initial update
+// updateWeatherDisplay();
+
+// // Update every 15 minutes (900,000ms)
+// setInterval(updateWeatherDisplay, 900000);
